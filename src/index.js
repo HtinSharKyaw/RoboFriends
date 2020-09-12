16af8f26c7b0cfd8a7714598a1b14e29b80de432
Htin Shar Kyaw <htinsharrkyaw@gmail.com>
2020-09-12 16:59:33 +0630

    first commit for robot Friends

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import Card  from './Card';
+import App from './containers/App'
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <App />
+    </div>
+ 
   </React.StrictMode>,
   document.getElementById('root')
 );
